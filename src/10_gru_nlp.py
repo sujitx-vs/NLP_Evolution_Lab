4f5f34fee218acd5e84f7b2d437ddcf7b8c59cd7
sujitx-vs <sujith.vs.cmfeb26@gmail.com>
2026-08-03 23:59:33 +0530

    small comments added to remove warning

--- src/10_gru_nlp.py
+++ src/10_gru_nlp.py
@@ -1,12 +1,12 @@
 import pandas as pd
-
+import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, GRU, Dense
+from tensorflow.keras.preprocessing.text import Tokenizer # type: ignore
+from tensorflow.keras.preprocessing.sequence import pad_sequences # type: ignore
+from tensorflow.keras.models import Sequential # type: ignore
+from tensorflow.keras.layers import Embedding, GRU, Dense   # type: ignore
 
 # ==========================================================
 # LOAD DATASET
